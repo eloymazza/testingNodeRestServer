8c51974942d79cad78a83038821f28dddd04f8a6
Eloy Mazza <eloymazza.ra@gmail.com>
2018-11-20 23:50:52 -0300

    Configure variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV == 'dev'){
     dbURL = 'mongodb://localhost:27017/cafe';
 }
 else{
-    dbURL = 'mongodb://cafeuser:altairezzio1@ds227119.mlab.com:27119/cafe'
+    dbURL = process.env.MONGO_URL
 }
 
 process.env.dbURL = dbURL;
